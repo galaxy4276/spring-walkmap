4ce07cda18254dd59e1e640ce6fe015dab269d90
galaxy4276 <galaxyhi4276@gmail.com>
2023-08-12 18:26:46 +0900

    feat: Configuration 의 자동 구성 정보 대체

--- toby-springboot-inflearn/src/main/java/config/autoconfig/TomcatWebServerConfig.java
+++ toby-springboot-inflearn/src/main/java/config/autoconfig/TomcatWebServerConfig.java
@@ -2,14 +2,17 @@ package config.autoconfig;
 
 import config.ConditionalMyOnClass;
 import config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
